91e4311711f5c55f6828f5bde603436e45a3d110
IlliterateElf <illiterate.elf@outlook.com>
2022-08-15 15:17:39 -0700

    check date length
    Commented out old function content.
    
    New function currently takes the date Updated field and date Online field and checks to see if they are over a month apart, and saves all indexes of those that are.

--- Maintanence/MaintanenceWeb/Home.js
+++ Maintanence/MaintanenceWeb/Home.js
@@ -14,14 +14,59 @@
         await Excel.run(async (context) => {
             let sheet = context.workbook.worksheets.getActiveWorksheet();
 
-            const range = sheet.getRange("F2:F87");
-            const conditionalFormat = range.conditionalFormats.add(
-                Excel.ConditionalFormatType.cellValue
-            );
+            let rangeUpdate = sheet.getRange("E2:E90");
+            rangeUpdate.load("values");
+            await context.sync();
+            let dateUpdate = [];
+            for (let i = 0; i < rangeUpdate.values.length; i++) {
+                let newdate = Date.parse(rangeUpdate.values[i]);
+                newdate = newdate / (1000*60*60*24*30);
+                dateUpdate.push(newdate);
+            }
 
-            // Set the font of negative numbers to red.
-            conditionalFormat.cellValue.format.fill.color = "red";
-            conditionalFormat.cellValue.rule = { formula1: "=0", operator: "GreaterThan" };
+            let rangeOnline = sheet.getRange("H2:H90");
+            rangeOnline.load("values");
+            await context.sync();
+            let dateOnline = [];
+            for (let i = 0; i < rangeOnline.values.length; i++) {
+                if (rangeOnline.values[i] != "Online") {
+                    let newdate = Date.parse(rangeOnline.values[i]);
+                    newdate = newdate / (1000 * 60 * 60 * 24 * 30);
+                    dateOnline.push(newdate);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+                }
+                else {
+                    let newdate = new Date();
+                    newdate = Date.parse(newdate);
+                    newdate = newdate / (1000 * 60 * 60 * 24 * 30);
+                    dateOnline.push(newdate);
+                }
+            }
+
+            let badIndexes = [];
+            for (let i = 0; i < dateUpdate.length; i++) {
+                if (dateOnline[i] - dateUpdate[i] > 1) {
+                    badIndexes.push(i);
+                }
+            }
+            //let sheet = context.workbook.worksheets.getActiveWorksheet();
+
+            //const rangeF = sheet.getRange("F2:F87");
+            //const conditionalFormatF = rangeF.conditionalFormats.add(
+            //    Excel.ConditionalFormatType.cellValue
+            //);
+
+            //// Set the fill of nonzeros to red.
+            //conditionalFormatF.cellValue.format.fill.color = "red";
+            //conditionalFormatF.cellValue.rule = { formula1: "=0", operator: "GreaterThan" };
+
+            //const rangeI = sheet.getRange("I2:I87");
+            //const conditionalFormatI = rangeI.conditionalFormats.add(
+            //    Excel.ConditionalFormatType.cellValue
+            //);
+
+            //// Set the fill of nonzero numbers to red.
+            //conditionalFormatI.cellValue.format.fill.color = "red";
+            //conditionalFormatI.cellValue.rule = { formula1: "=0", operator: "GreaterThan" };
 
             await context.sync();
         });
