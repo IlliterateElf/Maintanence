0d5a8e26a4ad88dc54aa8d31021e2fde20bb68ed
IlliterateElf <illiterate.elf@outlook.com>
2022-08-16 10:53:35 -0700

    begin modules checker
    Home.js now has the code for the modules checker. So far AntiMal, Network Attack Defense, Firewall and Content Control are checked. No fills are done just yet.

--- Maintanence/MaintanenceWeb/Home.js
+++ Maintanence/MaintanenceWeb/Home.js
@@ -14,46 +14,98 @@
         await Excel.run(async (context) => {
             let sheet = context.workbook.worksheets.getActiveWorksheet();
 
-            let rangeUpdate = sheet.getRange("E2:E90");
-            rangeUpdate.load("values");
+            //Check AntiMal
+            let range = sheet.getRange("D2:D87");
+            range.load("values");
             await context.sync();
-            let dateUpdate = [];
-            for (let i = 0; i < rangeUpdate.values.length; i++) {
-                let newdate = Date.parse(rangeUpdate.values[i]);
-                newdate = newdate / (1000 * 60 * 60 * 24 * 30);
-                dateUpdate.push(newdate);
+
+            let vals = range.values;
+            let badRanges =[];
+            for (let i = 0; i < vals.length; i++) {
+                if (vals[i] != "Enabled") {
+                    badRanges.push("D" + String(i + 2));
+                }
             }
 
-            let rangeOnline = sheet.getRange("H2:H90");
-            rangeOnline.load("values");
+            //Check Network Attack Defense
+            range = sheet.getRange("E2:E87");
+            range.load("values");
             await context.sync();
-            let dateOnline = [];
-            for (let i = 0; i < rangeOnline.values.length; i++) {
-                if (rangeOnline.values[i] != "Online") {
-                    let newdate = Date.parse(rangeOnline.values[i]);
-                    newdate = newdate / (1000 * 60 * 60 * 24 * 30);
-                    dateOnline.push(newdate);
-                }
-                else {
-                    let newdate = new Date();
-                    newdate = Date.parse(newdate);
-                    newdate = newdate / (1000 * 60 * 60 * 24 * 30);
-                    dateOnline.push(newdate);
+
+            vals = range.values;
+            for (let i = 0; i < vals.length; i++) {
+                if (vals[i] != "Enabled") {
+                    badRanges.push("E" + String(i + 2));
                 }
             }
 
-            let badIndexes = [];
-            for (let i = 0; i < dateUpdate.length; i++) {
-                if (dateOnline[i] - dateUpdate[i] > 1) {
-                    badIndexes.push(i);
+            //Check Firewall
+            range = sheet.getRange("G2:G87");
+            range.load("values");
+            await context.sync();
+
+            vals = range.values;
+            for (let i = 0; i < vals.length; i++) {
+                if (vals[i] != "Disabled") {
+                    badRanges.push("G" + String(i + 2));
                 }
             }
 
-            for (let i in badIndexes) {
-                let row = String(badIndexes[i] + 2);
-                let range = sheet.getRange("E" + row);
-                range.format.fill.color = "red";
+            //Check Content Control
+            range = sheet.getRange("H2:H87");
+            range.load("values");
+            await context.sync();
+
+            vals = range.values;
+            for (let i = 0; i < vals.length; i++) {
+                if (vals[i] != "Enabled") {
+                    badRanges.push("H" + String(i + 2));
+                }
             }
+
+            console.log(badRanges);
+            //let sheet = context.workbook.worksheets.getActiveWorksheet();
+
+            //let rangeUpdate = sheet.getRange("E2:E90");
+            //rangeUpdate.load("values");
+            //await context.sync();
+            //let dateUpdate = [];
+            //for (let i = 0; i < rangeUpdate.values.length; i++) {
+            //    let newdate = Date.parse(rangeUpdate.values[i]);
+            //    newdate = newdate / (1000 * 60 * 60 * 24 * 30);
+            //    dateUpdate.push(newdate);
+            //}
+
+            //let rangeOnline = sheet.getRange("H2:H90");
+            //rangeOnline.load("values");
+            //await context.sync();
+            //let dateOnline = [];
+            //for (let i = 0; i < rangeOnline.values.length; i++) {
+            //    if (rangeOnline.values[i] != "Online") {
+            //        let newdate = Date.parse(rangeOnline.values[i]);
+            //        newdate = newdate / (1000 * 60 * 60 * 24 * 30);
+            //        dateOnline.push(newdate);
+            //    }
+            //    else {
+            //        let newdate = new Date();
+            //        newdate = Date.parse(newdate);
+            //        newdate = newdate / (1000 * 60 * 60 * 24 * 30);
+            //        dateOnline.push(newdate);
+            //    }
+            //}
+
+            //let badIndexes = [];
+            //for (let i = 0; i < dateUpdate.length; i++) {
+            //    if (dateOnline[i] - dateUpdate[i] > 1) {
+            //        badIndexes.push(i);
+            //    }
+            //}
+
+            //for (let i in badIndexes) {
+            //    let row = String(badIndexes[i] + 2);
+            //    let range = sheet.getRange("E" + row);
+            //    range.format.fill.color = "red";
+            //}
             //let sheet = context.workbook.worksheets.getActiveWorksheet();
 
             //const rangeF = sheet.getRange("F2:F87");
